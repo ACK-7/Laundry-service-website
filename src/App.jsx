fca1f33931d889feeb306c5d130a66347c0461f6
Asiima Crisen <asiimac3@gmail.com>
2025-10-28 01:48:59 +0300

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import ContactSection from './component/ContactSection';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           {/* Add more routes as needed */}
+          <Route path="/contact" element={<ContactSection />} />
         </Routes>
       </div>
     </Router>
